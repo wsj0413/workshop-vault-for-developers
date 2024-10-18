7c8e07f0117d6d5a0858b569e1427dbd4bcf7bb2
wsj0413 <M.wushujun@outlook.com>
2024-10-19 00:40:09 +0800

    Update minikube.sh

--- kubernetes/scripts/minikube.sh
+++ kubernetes/scripts/minikube.sh
@@ -2,6 +2,9 @@
 
 set -e -o pipefail
 
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
 minikube start --driver=docker
 #minikube start --driver=docker --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1
 
