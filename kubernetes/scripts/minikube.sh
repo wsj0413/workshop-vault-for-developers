4e309dd26a4654938eb157d25eea18dcdf02bf5c
wsj0413 <M.wushujun@outlook.com>
2024-10-19 00:02:55 +0800

    Update minikube.sh

--- kubernetes/scripts/minikube.sh
+++ kubernetes/scripts/minikube.sh
@@ -3,6 +3,8 @@
 set -e -o pipefail
 
 minikube start --driver=docker
+#minikube start --driver=docker --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1
+
 
 eval $(minikube -p minikube docker-env)
 cd ../payments-database && docker build -t payments-database .
